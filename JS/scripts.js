62a46e6e20be1db0016a96395cce8d318df58376
HubertasVin <hubertas2003@gmail.com>
2021-01-13 20:46:51 +0200

    keli pakeitimai

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,10 +1,12 @@
 function showPopup(popupId) {
     document.getElementById(popupId).style.visibility = "visible";
+    document.getElementById(popupId).style.opacity = "1";
     document.body.style.overflow = "hidden";
 }
 
 function hidePopup(popupId) {
     document.getElementById(popupId).style.visibility = "hidden";
+    document.getElementById(popupId).style.opacity = "0";
     document.body.style.overflow = "visible";
 }
 
