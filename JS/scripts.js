b3281b08e7aef17d7fb1162cda400267f3ff8857
FluffyDango <renaldas1251@gmail.com>
2021-01-18 17:44:39 +0200

    Darbas kartu

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,3 +1,10 @@
+let images = ['../images/Smokes/Overpass/overpass_monster1_after.jpg', '../images/Smokes/overpass_monster1_before1.jpg', '../images/Smokes/Overpass/overpass_monster1_before2.jpg']
+let index = 0;
+
+let next = document.getElementById('nextImg');
+let prev = document.getElementById('prevImg');
+
+
 function showPopup(popupId) {
     document.getElementById(popupId).style.visibility = "visible";
     document.getElementById(popupId).style.opacity = "1";
@@ -17,3 +24,23 @@ document.addEventListener("keydown", event => {
         }
 }, false);
 
+
+next.addEventListener('click', nextImage);
+
+function nextImage() {
+    if(index < images.length - 1) {
+        for (i = 0; i < 10; i++) {
+            document.getElementById('imgSrc').src = images[index++];
+        }
+    }
+}
+
+prev.addEventListener('click', previousImage);
+
+function previousImage() {
+    if(index > 0) {
+        for (i = 0; i < 10; i++) {
+            document.getElementById('imgSrc').src = images[index--];
+        }
+    }
+}
