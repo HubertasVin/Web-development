ab7f84adc128cb1e3b7854db6f3343efffc42623
HubertasVin <hubertas2003@gmail.com>
2021-01-18 19:39:30 +0200

    įdėta nuotraukų galerijos funkcija

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,4 @@
-let images = ['../images/Smokes/Overpass/overpass_monster1_after.jpg', '../images/Smokes/overpass_monster1_before1.jpg', '../images/Smokes/Overpass/overpass_monster1_before2.jpg']
+let images = ['../images/Smokes/Overpass/overpass_monster1_after.jpg', '../images/Smokes/Overpass/overpass_monster1_before1.jpg', '../images/Smokes/Overpass/overpass_monster2_before2.jpg']
 let index = 0;
 
 let next = document.getElementById('nextImg');
@@ -24,23 +24,22 @@ document.addEventListener("keydown", event => {
         }
 }, false);
 
-
-next.addEventListener('click', nextImage);
-
+// Nuotrauku galerijos next ir previous mygtukai
 function nextImage() {
     if(index < images.length - 1) {
-        for (i = 0; i < 10; i++) {
-            document.getElementById('imgSrc').src = images[index++];
-        }
+        index++;
+    } else {
+        index = 0;
     }
+    document.getElementById('imgSrc').src = images[index];
 }
 
-prev.addEventListener('click', previousImage);
 
 function previousImage() {
     if(index > 0) {
-        for (i = 0; i < 10; i++) {
-            document.getElementById('imgSrc').src = images[index--];
-        }
+        index--;
+    } else {
+        index = images.length - 1;
     }
+    document.getElementById('imgSrc').src = images[index];
 }
