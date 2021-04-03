137e8145d27b4ecb3fcad5dc3be74f308f49461c
HubertasVin <hubertas2003@gmail.com>
2021-04-03 14:08:49 +0300

    smoke'u loadinimas pagal id

--- JS/JSONImageLoader.js
+++ JS/JSONImageLoader.js
@@ -3,31 +3,30 @@
 let images;
 let instructions;
 
-function getDataImg(loc) {
+function getDataImg(loc, id) {
     fetch("../JSON/smokeInfo.json")
         .then((resp) => {
             return resp.json();
         })
         .then((data) => {
             if (localStorage.getItem('currentMap') === "mirage") {
-                mirageGalleryLoad(loc, data);
+                mirageGalleryLoad(loc, id, data);
+            } else {
+
             }
         });
 }
 
-function mirageGalleryLoad(loc, data) {
-    if (loc == 0) {
-        images = data.mirage.bench[loc].img;
-        instructions = data.mirage.bench[loc].thrdesc;
+function mirageGalleryLoad(loc, id, data) {
+    images = data.mirage[loc].smokes[id].img;
+    instructions = data.mirage[loc].smokes[id].thrdesc;
 
-        img.src = images[0];
+    document.getElementById('imgSrc').src = images[0];
 
-        // šits sutvarko popup. Kažkur kitur reiks numest
-        // panaikina praeitus smokebox
-        document.getElementById('JSONinfo').innerHTML = "";
-        // įdeda rodyklytes
-        document.getElementById('popupPhoto').innerHTML += "<a class='fas fa-chevron-left imgPrev' id='nextImg' onclick='previousImage()'></a><a class='fas fa-chevron-right imgNext' id='prevImg' onclick='nextImage()'></a><div class='smokeDescription'><div class='watchVideo popupText' id='playVid' onclick='playVideo()'><i class='fas fa-play'></i> watch the video</div><p class='smokeInstructions' id='smokeInstructions'><p></div>";
+    // šits sutvarko popup. Kažkur kitur reiks numest
+    // įdeda rodyklytes
+    document.getElementById('popupPhoto').innerHTML += "<a class='fas fa-chevron-left imgPrev' id='nextImg' onclick='previousImage()'></a><a class='fas fa-chevron-right imgNext' id='prevImg' onclick='nextImage()'></a><div class='smokeDescription'><div class='watchVideo popupText' id='playVid' onclick='playVideo()'><i class='fas fa-play'></i> watch the video</div><p class='smokeInstructions' id='smokeInstructions'><p></div>";
         
-        document.getElementById('smokeInstructions').innerHTML = instructions[0];
-    }
+    document.getElementById('smokeInstructions').innerHTML = instructions[0];
+    
 }
--- JS/JSONPopupHandler.js
+++ JS/JSONPopupHandler.js
@@ -13,34 +13,21 @@ function getData(loc) {
 }
 
 function mirageSmokeLoad(loc, data) {
-    if (loc == 0) {
-        let result = "";
-        for (let i = 0; i < Object.keys(data.mirage.bench).length; i++) {
-            if (data.mirage.bench[i].oneway == true) {
-            result += "<div class='smokeBoxMap' id='smokeBoxMap' onclick='getDataImg(" + i + ")'><h3>" + data.mirage.bench[i].from + "<i class='fas fa-cloud fa-1x'></i></h3><h4><i class='fas fa-arrow-alt-circle-up fa-1x' title='One-Way Smoke'></i></h4><img src='" + data.mirage.bench[i].img[1] + "'></img></div>";
-            } else {
-                result += "<div class='smokeBoxMap' id='smokeBoxMap' onclick='getDataImg(" + i + ")'><h3>" + data.mirage.bench[i].from + "<i class='fas fa-cloud fa-1x'></i></h3><h4><div class='placeholder'></div></h4><img src='" + data.mirage.bench[i].img[1] + "'></img></div>";
-            }
+    let result = "";
+    for (let i = 0; i < Object.keys(data.mirage[loc].smokes).length; i++) {
+        result += "<div class='smokeBoxMap' id='smokeBoxMap' onclick='getDataImg(" + loc + ";" + i + ")'><h3>" + data.mirage[loc].smokes[i].from + "<i class='fas fa-cloud fa-1x'></i></h3><h4>";
+
+        if (data.mirage[loc].smokes[i].oneway == true) {
+            result += "<i class='fas fa-arrow-alt-circle-up fa-1x' title='One-Way Smoke'></i></h4><img src='" + data.mirage[loc].smokes[i].img[1] + "'></img></div>";
+        } else {
+            result += "<div class='placeholder'></div></h4><img src='" + data.mirage[loc].smokes[i].img[1] + "'></img></div>";
         }
-        document.getElementById('JSONinfo').innerHTML = result;
     }
-    // if (loc === "alleyBlock") {
-    //     let result = "";
-    //     for (let i = 0; i < Object.keys(data.mirage[1].alleyBlock).length; i++) {
-    //         //One-Way Smoke'ai
-    //         if (data.mirage[1].alleyBlock[i].oneway == true) {
-    //         result += "<div class='smokeBoxMap' id='smokeBoxMap' onclick='showPopup(&quot;smokePopup&quot;); hidePopup(&quot;mainPopup&quot;, false); getDataImg(&quot;alleyBlock&quot;, " + i + ")'><h3>" + data.mirage[1].alleyBlock[i].from + "<i class='fas fa-cloud fa-1x'></i></h3><h4><i class='fas fa-arrow-alt-circle-up fa-1x' title='One-Way Smoke'></i></h4><img src='" + data.mirage[1].alleyBlock[i].img[1] + "'></img></div>";
-    //         } else {
-    //             result += "<div class='smokeBoxMap' id='smokeBoxMap' onclick='showPopup(&quot;smokePopup&quot;); hidePopup(&quot;mainPopup&quot;, false); getDataImg(&quot;alleyBlock&quot;, " + i + ")'><h3>" + data.mirage[1].alleyBlock[i].from + "<i class='fas fa-cloud fa-1x'></i></h3><h4><div class='placeholder'></div></h4><img src='" + data.mirage[1].alleyBlock[i].img[1] + "'></img></div>";
-    //         }
-    //     }
-    //     document.getElementById('JSONinfo').innerHTML = result;
-    // }
+    document.getElementById('JSONinfo').innerHTML = result;
 }
 
 
-//clear the popup - NEVEIKIA
-// function clearPopup(id) {
-//     let div = document.getElementById(id);
-//     div.remove();
-// }
+function clearPopup(id) {
+    const div = document.getElementById(id);
+    div.innerHTML = "";
+}
