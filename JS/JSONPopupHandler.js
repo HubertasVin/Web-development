5376c2cbbb47da991aa5393b216b57f52e49590a
FluffyDango <renaldas1251@gmail.com>
2021-04-13 10:07:06 +0300

    Tickrate filtrai veikia

--- JS/JSONPopupHandler.js
+++ JS/JSONPopupHandler.js
@@ -17,8 +17,10 @@ function getData(loc) {
         });
 }
 
+let result;
+
 function smokeLoad(loc) {
-    let result = "";
+    result = "";
 
     if (typeof map[loc] === "undefined") {
         document.getElementById('smokeTitle').innerHTML = "undefined";
@@ -27,28 +29,40 @@ function smokeLoad(loc) {
         document.getElementById('smokeTitle').innerHTML = map[loc].title + " " + localStorage.getItem('typeActive');
     
         for (let number = 1; number < Object.keys(map[loc]).length; number++) {
-            result += "<div class='smokeBoxMap' id='smokeBoxMap' ";
-
-            // iškart užkraus video jeigu per settings pasirinkai  taip
-            if (localStorage.getItem('loadVideoFirst') == "false") {
-                result += "onclick='getDataImg(" + loc + "," + number + ")'";
-            } else {
-                result += "onclick='playVideo(" + loc + "," + number + ")'";
+            if (localStorage.getItem('tick1') === "true" && map[loc][number].tickrate.includes("64")) {
+                loadSmokeBoxMap(loc, number);
             }
-
-            // smoke'o dėžės pavadinimas
-            result += "><h3>" + "from\ " + map[loc][number].from;
-
-            // ar one way
-            if (map[loc][number].oneway == true) {
-                result += "<i class='fas fa-arrow-alt-circle-up fa-1x' title='One-Way Smoke'></i>";
+            else  if (localStorage.getItem('tick2') === "true" && map[loc][number].tickrate.includes("128")) {
+                loadSmokeBoxMap(loc, number);
             }
-
-            // nuotrauka ir views
-            result += "</h3><i class='fas fa-cloud fa-10x'></i><img src='" + map[loc][number].img[0] + "'></img><h3 id='views'>views</h3></div>";
+            else if (map[loc][number].tickrate.includes("128 64")) {
+                loadSmokeBoxMap(loc, number);
+            }
         }
         document.getElementById('JSONinfo').innerHTML = result;
     }
+}
+
+function loadSmokeBoxMap (loc, number) {
+    result += "<div class='smokeBoxMap' id='smokeBoxMap' ";
+
+    // iškart užkraus video jeigu per settings pasirinkai  taip
+    if (localStorage.getItem('loadVideoFirst') == "false") {
+        result += "onclick='getDataImg(" + loc + "," + number + ")'";
+    } else {
+        result += "onclick='playVideo(" + loc + "," + number + ")'";
+    }
+
+    // smoke'o dėžės pavadinimas
+    result += "><h3>" + "from\ " + map[loc][number].from;
+
+    // ar one way
+    if (map[loc][number].oneway == true) {
+        result += "<i class='fas fa-arrow-alt-circle-up fa-1x' title='One-Way Smoke'></i>";
+    }
+
+    // nuotrauka ir views
+    result += "</h3><i class='fas fa-cloud fa-10x'></i><img src='" + map[loc][number].img[0] + "'></img><h3 id='views'>views</h3></div>";
 }
 
 function clearPopup() {
